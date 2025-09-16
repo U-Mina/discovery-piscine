54fcb060f53fdaf30f2a2c947c8c6937369fa82c
U-Mina <erya.wuu@gmail.com>
2025-09-16 08:55:56 +0200

    js script for cell03

---
+++ cell03/ex00/script.js
@@ -0,0 +1,13 @@
+// hex clr format
+function createRandomClr() {
+    const s = "0123456789ABCDEF";
+    let clr = "#";
+    for (let i = 0; i < 6; i++) {
+        clr += s[Math.floor(Math.random() * 16)];
+    }
+    return clr;
+}
+
+document.getElementById("colorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = createRandomClr();
+});
